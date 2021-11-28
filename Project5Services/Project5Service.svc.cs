e8765cdd5ed9472aaeb1327fe929b91f30819450
samarthi894 <90678416+samarthi894@users.noreply.github.com>
2021-11-27 19:02:59 -0700

    Update Project5Service.svc.cs

--- Project5Services/Project5Service.svc.cs
+++ Project5Services/Project5Service.svc.cs
@@ -48,5 +48,85 @@ namespace Project5Services
 
             return "Failed to send invite";
         }
+        
+        
+        // Get show timings
+        
+        public IDictionary<string, string[]> GetShowTimings(string filmname)
+        {
+            string date_time = DateTime.UtcNow.AddDays(1).ToString("yyyy-MM-ddTHH:mm:ssZ");
+            var client = new RestClient("https://api-gate2.movieglu.com/filmsNowShowing/?n=25"); // service call to fetch the current movies whicg=h are playing.
+            IDictionary<string, string> films = new Dictionary<string, string>(); // dictionary to store the film names.
+            DateTime utcTime = DateTime.UtcNow;
+
+            client.Timeout = -1;
+            var request = new RestRequest(Method.GET);
+            request.AddHeader("api-version", "v200");
+            request.AddHeader("Authorization", "Basic U1RVRF8xOTA6RGFSbnB5eWpkYjdQ");
+            request.AddHeader("client", "STUD_190");
+            request.AddHeader("x-api-key", "bs6f0mXzgo5r1vYuUj8Vj9cwiYP4tqVK9ShZFLlB");
+            request.AddHeader("device-datetime", date_time);
+            request.AddHeader("territory", "US");
+            IRestResponse response = client.Execute(request);
+            dynamic film_lst = JObject.Parse(response.Content); // getting lits of films which are currently playing in theatres.
+
+
+            foreach (var item in film_lst.films)
+            {
+                string filmid = item.film_id;
+                string filmnames = item.film_name;     // looping through the list of films and adding it to the dictionary with key as fillname and value as filmid.
+                films.Add(filmnames.ToLower(), filmid);
+            }
+
+
+
+            var dateString2 = DateTime.Now.ToString("yyyy-MM-dd");
+
+            bool flag = films.ContainsKey(filmname.ToLower());
+
+            if (flag)
+            {
+
+                var obj = new RestClient("https://api-gate2.movieglu.com/filmShowTimes/?film_id=" + films[filmname] + "&date=" + dateString2 + "&n=5"); // service call to get the movie show times based on the film id.
+                obj.Timeout = -1;
+                var request_2 = new RestRequest(Method.GET);
+                request_2.AddHeader("api-version", "v200");
+                request_2.AddHeader("Authorization", "Basic U1RVRF8xOTA6RGFSbnB5eWpkYjdQ");
+                request_2.AddHeader("client", "STUD_190");
+                request_2.AddHeader("x-api-key", "bs6f0mXzgo5r1vYuUj8Vj9cwiYP4tqVK9ShZFLlB");
+                request_2.AddHeader("device-datetime", date_time);
+                request_2.AddHeader("territory", "US");
+                request_2.AddHeader("geolocation", "33.42192;-111.8798");
+                IRestResponse response_2 = obj.Execute(request_2);
+                dynamic film_json = JObject.Parse(response_2.Content);
+
+                IDictionary<string, string[]> movies = new Dictionary<string, string[]>();
+
+                foreach (var item in film_json.cinemas)
+                {
+                    List<string> lst = new List<string>();
+                    string cinemaname = item.cinema_name;
+
+                    foreach (var i in item.showings.Standard.times)
+                    {
+                        string time = "start_time: " + i.start_time + ", end_time: " + i.end_time;
+                        lst.Add(time);
+
+                    }
+                    string[] lst_arr = lst.ToArray();
+                    movies.Add(cinemaname, lst_arr);
+
+                }
+
+                return movies; // returning the theatre names and their corresponding show times.
+            }
+            else
+            {
+                return null;
+            }
+
+
+        }
+
     }
 }
